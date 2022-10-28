9bbc14c4fa7953025a74bea777dbfb5a67c5daf6
Nekxis <nexispolska16@gmail.com>
2022-10-28 23:00:27 +0200

    fix

--- launchero-client/pages/index.js
+++ launchero-client/pages/index.js
@@ -3,19 +3,16 @@ import MenuBar from "../src/components/menubar/MenuBar";
 import Meal from '../src/components/meals-section/Meal';
 export default function Home() {
   return (
-    <div className={'flex w-full items-center justify-center flex-col'}>
+    <div className={'flex w-full h-screen items-center justify-center flex-col'}>
       <Navbar />
-      <MenuBar />
-      <div className='border border-black rounded-md  w-3/4'>
-        <ul>
-          <li className="w-full rounded-md">
-            <Meal />
-            <Meal />
-          </li>
-        </ul>
-        
-
+      <div className={'flex h-full w-full justify-center self-center'}>
+        <MenuBar />
       </div>
+
+
+
+
+
     </div>
 
   )
--- launchero-client/src/components/menubar/MenuBar.js
+++ launchero-client/src/components/menubar/MenuBar.js
@@ -1,16 +1,18 @@
+import Meal from "../meals-section/Meal";
 
 export default function MenuBar () {
   return (
-    <div className={'flex flex-col p-5 w-full h-1/3 justify-center self-center border'}>
-      <div className={'flex w-full h-1/5 self-center justify-around'}>
+    <div className={'flex flex-col p-5 w-3/4 h-2/3 justify-center self-center border'}>
+      <div className={'flex w-full h-20 self-center justify-around'}>
         <button className={'flex justify-center self-center items-center w-1/5 focus:border border-b-0 focus:rounded-t-lg h-full'}>M</button>
         <button className={'flex justify-center self-center items-center w-1/5 focus:border border-b-0 focus:rounded-t-lg h-full'}>T</button>
         <button className={'flex justify-center self-center items-center w-1/5 focus:border border-b-0 focus:rounded-t-lg h-full'}>W</button>
         <button className={'flex justify-center self-center items-center w-1/5 focus:border border-b-0 focus:rounded-t-lg h-full'}>T</button>
         <button className={'flex justify-center self-center items-center w-1/5 focus:border border-b-0 focus:rounded-t-lg h-full'}>F</button>
       </div>
-      <div className={'flex w-full h-full self-center border overflow-scroll'}>
-
+      <div className={'flex flex-col w-full h-full self-center border overflow-scroll rounded-md'}>
+            <Meal />
+            <Meal />
       </div>
     </div>
   )
