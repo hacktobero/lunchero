5c0854947d78e5ae18965706b7c6928937784e44
pawelcp <94795184+pawelcp@users.noreply.github.com>
2022-10-28 22:31:58 +0200

    123

--- launchero-client/pages/index.js
+++ launchero-client/pages/index.js
@@ -1,9 +1,10 @@
-
+import Navbar from "../src/components/navbar";
 import MenuBar from "../src/components/menubar/MenuBar";
 import Meal from '../src/components/meals-section/Meal';
 export default function Home() {
   return (
     <div className={'flex w-full items-center justify-center flex-col'}>
+      <Navbar />
       <MenuBar />
       <div className='border border-black rounded-md  w-3/4'>
         <ul>
