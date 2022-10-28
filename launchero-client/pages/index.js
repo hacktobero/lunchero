2ec68cc276b2cfd4a3ddadc7ba936ee801020556
Nekxis <nexispolska16@gmail.com>
2022-10-28 22:19:03 +0200

    menubar

--- launchero-client/pages/index.js
+++ launchero-client/pages/index.js
@@ -2,7 +2,7 @@ import MenuBar from "../src/components/menubar/MenuBar";
 
 export default function Home() {
   return (
-    <div className={'flex w-full h-screen'}>
+    <div className={'flex w-full h-screen p-2'}>
       <MenuBar />
     </div>
   )
