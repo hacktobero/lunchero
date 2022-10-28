d7714d19844b6eb788733e1ad0b851c0e1fac3d5
pawelcp <94795184+pawelcp@users.noreply.github.com>
2022-10-28 23:51:39 +0200

    123

--- launchero-client/src/components/menubar/MenuBar.js
+++ launchero-client/src/components/menubar/MenuBar.js
@@ -5,11 +5,11 @@ export default function MenuBar () {
 
     <div className={'flex flex-col p-5 w-3/4 h-2/3 justify-center self-center shadow-xl'}>
       <div className={'flex w-full h-20 self-center justify-around'}>
-        <button className={'flex justify-center self-center items-center w-1/5 focus:border border-b-0 focus:rounded-lg h-full'}>Mon</button>
-        <button className={'flex justify-center self-center items-center w-1/5 focus:border border-b-0 focus:rounded-lg h-full'}>Tue</button>
-        <button className={'flex justify-center self-center items-center w-1/5 focus:border border-b-0 focus:rounded-lg h-full'}>Wed</button>
-        <button className={'flex justify-center self-center items-center w-1/5 focus:border border-b-0 focus:rounded-lg h-full'}>Thu</button>
-        <button className={'flex justify-center self-center items-center w-1/5 focus:border border-b-0 focus:rounded-lg h-full'}>Fri</button>
+        <button className={'flex justify-center shadow-sm self-center items-center w-1/5 focus:border border-b-0 focus:rounded-lg h-full'}>Mon</button>
+        <button className={'flex justify-center shadow-sm self-center items-center w-1/5 focus:border border-b-0 focus:rounded-lg h-full'}>Tue</button>
+        <button className={'flex justify-center shadow-sm self-center items-center w-1/5 focus:border border-b-0 focus:rounded-lg h-full'}>Wed</button>
+        <button className={'flex justify-center shadow-sm self-center items-center w-1/5 focus:border border-b-0 focus:rounded-lg h-full'}>Thu</button>
+        <button className={'flex justify-center shadow-sm self-center items-center w-1/5 focus:border border-b-0 focus:rounded-lg h-full'}>Fri</button>
       </div>
       <div className={'flex flex-col w-full h-full self-center overflow-y-scroll rounded-md'}>
             <Meal />
