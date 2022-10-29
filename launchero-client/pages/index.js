75277086b72ad568376e06a7237983fbb176bc77
Nekxis <nexispolska16@gmail.com>
2022-10-29 15:56:10 +0200

    context

--- launchero-client/pages/index.js
+++ launchero-client/pages/index.js
@@ -1,13 +1,15 @@
 import Navbar from "../src/components/navbar";
 import MenuBar from "../src/components/menubar/MenuBar";
-import Meal from '../src/components/meals-section/Meal';
+
 export default function Home() {
+
   return (
     <div className={'flex w-full h-screen items-center justify-center flex-col'}>
       <Navbar />
       <div className={'flex h-full w-full justify-center self-center'}>
         <MenuBar />
       </div>
+
 
 
 
