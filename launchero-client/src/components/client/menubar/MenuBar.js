3540ced4ed11997dd8655006fae91a2be968961e
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2022-10-29 15:09:20 +0200

    Update MenuBar.js

--- launchero-client/src/components/client/menubar/MenuBar.js
+++ launchero-client/src/components/client/menubar/MenuBar.js
@@ -1,5 +1,16 @@
 import Meal from "../meals-section/Meal";
+import Tab from "./Tab";
+import { useState } from 'react';
+export default function MenuBar() {
 
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+
+  const [activeDay, setActiveDay] = useState('');
+
+  const setDayHandler = (day) => {
+    setActiveDay(day);
+  }
   return (
 
     <div className={'flex flex-col p-5 w-3/4 h-2/3 justify-center self-center shadow-xl'}>
