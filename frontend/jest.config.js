0584bb2e9e2d697ffeeb22ea7d22128a6dcb23ea
Paweł Biegun <pawel.biegun@flyrlabs.com>
2022-10-31 16:35:27 +0100

    finish

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -14,6 +14,12 @@ const customJestConfig = {
     // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
+    collectCoverage: true,
+    coverageReporters: ["html"],
+    collectCoverageFrom: [
+        'src/**/*.{js,jsx}',
+        'pages/**/*.{js,jsx}',
+    ],
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
