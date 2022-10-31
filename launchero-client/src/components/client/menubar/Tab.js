16a7de1bd94d8c58a20242844d6a8c740ecb9fdf
pawelcp <94795184+pawelcp@users.noreply.github.com>
2022-10-31 15:52:45 +0100

    123

--- launchero-client/src/components/client/menubar/Tab.js
+++ launchero-client/src/components/client/menubar/Tab.js
@@ -1,6 +1,6 @@
 
 export default function Tab({ day, onDayChange, active }) {
     return (
-        <button onClick={() => { onDayChange(day) }} className={`flex duration-200 text-white font-bold justify-center shadow-sm self-center items-center w-1/5 rounded-t-2xl h-full ${active === day ? `bg-green-700 hover:bg-green-700 border border-gray text-2xl` : 'bg-green-400 border text-xl'}`}>{day}</button>
+        <button onClick={() => { onDayChange(day) }} className={`flex duration-200 text-white font-bold justify-center shadow-sm self-center items-center w-1/5 rounded-t-2xl rounded-b-sm h-full ${active === day ? `bg-green-700 hover:bg-green-700 border border-gray text-2xl` : 'bg-green-400 border text-xl'}`}>{day}</button>
     )
 }
--- launchero-client/src/components/operator/operatorView/OperatorAddMenu.js
+++ launchero-client/src/components/operator/operatorView/OperatorAddMenu.js
@@ -1,33 +1,26 @@
-import Navbar from "../navbar"
+import Navbar from "../../navbar"
 import { useState } from "react";
-import Tab from "../client/menubar/Tab";
-import Meal from "./Meal";
-import { IoMdAdd } from 'react-icons/io';
+import Tab from "../../client/menubar/Tab";
+import Meal from "../Meal";
 
-const Operator = () => { 
+const AddMenu = () => { 
 
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
-
     const [activeDay, setActiveDay] = useState('');
-    const [mealName, SetMealName] = useState('')
 
     const setDayHandler = (day) => {
-      setActiveDay(day);
-    }
+        setActiveDay(day);
+      }
 
-    const meals = {
+      const meals = {
         name: 'essa',
         ingredients: 'esaas',
         type:'',
         preferences:''
       }
     return(
-        <div className={"flex flex-col"}>
-            <div className="flex">
-                <Navbar></Navbar>
-            </div>
-            <div className="w-8/10 self-center justify-around p-10 shadow-lg mt-20 rounded-xl">
+        <div className="w-8/10 self-center justify-around p-10 shadow-lg mt-20 rounded-xl">
                 <div className={'flex  h-20'}>
                     {days.map((day, index) => {
                     return (
@@ -39,8 +32,6 @@ const Operator = () => {
                      <Meal meals={meals}></Meal>
                  </div>
             </div>
-        </div>
     )
  }
-
- export default Operator
+ export default AddMenu
