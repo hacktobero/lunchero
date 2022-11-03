dbf0280b4a0422f930ffeff987b57428510bd9fd
Paweł Biegun <pawel.biegun@flyrlabs.com>
2022-11-03 07:24:36 +0100

    finish

--- backend/utils/database_utils.py
+++ backend/utils/database_utils.py
@@ -1,10 +1,10 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    os.environ["DB_CONNECTION_URL"]
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
