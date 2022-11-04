e332fe830536c19f9ec47dbe5e170fa4cb1f40db
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2022-11-04 20:47:41 +0100

    e

--- frontend/__tests__/Navbar.test.js
+++ frontend/__tests__/Navbar.test.js
@@ -1,7 +1,7 @@
 import Navbar from "../src/components/navbar";
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
-
+import { act } from 'react-dom/test-utils';
 describe('Navbar component', () => {
 
     test('renders correct name of the company', () => {
@@ -9,15 +9,24 @@ describe('Navbar component', () => {
         const companyNameElement = screen.getByText('Lunchero');
         expect(companyNameElement).toBeInTheDocument();
     });
+
     test('renders correct photo of the currently logged in user', async () => {
         render(<Navbar />);
         const userPictureElement = await screen.findByRole('img');
         expect(userPictureElement).toBeInTheDocument();
     });
-    test('renders alert if the user is not logged in ', async () => {
+
+    test('renders correct name if the user is not currently logged in', () => {
         render(<Navbar />);
-        const userElement = await screen.findByText('You are not currently logged in');
-        expect(userElement).toBeInTheDocument();
+        const userNameElement = screen.getByTestId('username').textContent;
+        expect(userNameElement).toMatch('You are currently not logged in');
     });
+
+    test('renders correct name of the user that is currently logged in', async () => {
+        render(<Navbar />)
+        const userNameElement = await screen.findByText('pawelsdjjd2');
+        expect(userNameElement).to
+    });
+
 
 });
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react"
-import { MealsContext } from "../Context/Context"
+
 const Navbar = () => {
 
     const [data, setData] = useState([]);
 
     useEffect(() => {
         const getData = async () => {
-          const res = await fetch("/data.json");
-          const data = await res.json();
-          setData(data)
+            try {
+                const res = await fetch("/data.json");
+                const data = await res.json();
+                setData(data)
+            } catch (e) {
+                console.log(e.message);
+            }
         };
         getData();
     }, []);
@@ -24,8 +28,7 @@ const Navbar = () => {
         <div className=" w-full h-1/12 flex  md:flex shadow-lg">
             <div className='flex lg:mx-10 lg:my-4 lg:p-8 sm:p-3 sm:my-0 lg:w-28  sm:w-20 rounded flex-col cursor-pointer ease-in-out duration-150 '>
                 <img className="shadow-lg  h-full ml-8 flex self-center rounded-full " src={profilePhoto[0].photo}></img>
-                {!user && <h2 className='w-full sm:ml-1 lg:ml-0 lg:text-xl sm:text-lg text-center'>You are not currently logged in</h2>}
-                {user && <h2 className='w-full sm:ml-1 lg:ml-0 lg:text-xl sm:text-lg text-center'>{user.username}</h2>}
+                <h2 data-testid='username' className='w-full sm:ml-1 lg:ml-0 lg:text-xl sm:text-lg text-center'>{`${user ? `${user.username}` : 'You are currently not logged in'}`}</h2>
             </div>
             <div className='text-5xl w-2/3 flex justify-end items-center'>
                 <h1 className=' text-green-400 font-bold'>Lunchero</h1>
