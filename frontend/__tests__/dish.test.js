920f467dfb0d87ba8c3821af9a55c52ac48cdaae
Nekxis <nexispolska16@gmail.com>
2022-11-05 16:39:15 +0100

    clean version

--- frontend/__tests__/dish.test.js
+++ frontend/__tests__/dish.test.js
@@ -6,42 +6,19 @@ import Dish from "../src/components/client/meals-section/Dish";
 
 
 describe('if dish counter work', () => {
-//  function items(cartItems) {
-//   render(
-//     <MealsContext.Provider value={{cartItems}} >
-//       <Dish />
-//     </MealsContext.Provider> )
-//  }
 
   const setState = jest.fn();
 
 
   test('+ button increase quantity', () => {
-    let cartItems = [];
-    const setCartItems = jest.fn(()=>cartItems.push({id:10,getItemQuantity:1}));
 
-
-    const increaseCartQuantity = jest.fn((id)=>  {
-      setCartItems (currentItems => {
-      if (currentItems.find(item => item.id === id) == null) {
-        return [...currentItems, {id, quantity:1}]
-      } else {
-        return currentItems.map(item => {
-          if (item.id === id) {
-            return {...item, quantity: item.quantity + 1}
-          } else {
-            return item
-          }
-        })
-      }
-    })
-    })
-    const decreaseCartQuantity = jest.fn()
-    const getItemQuantity = jest.fn((id)=> cartItems?.find(item => item.id === id)?.quantity || 0)
+    const increaseCartQuantity = jest.fn(() => {})
+    const decreaseCartQuantity = jest.fn(() => {})
+    const getItemQuantity = jest.fn(() => {})
 
     render(
       <MealsContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity }} >
-        <Dish />
+        <Dish id={10} />
       </MealsContext.Provider> )
 
 
