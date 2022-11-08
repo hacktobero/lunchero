21669e469f994aba2d49abb9016597fdb688d2e4
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2022-11-08 16:51:33 +0100

    Update tab.test.js

--- frontend/__tests__/tab.test.js
+++ frontend/__tests__/tab.test.js
@@ -1,13 +1,33 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import Tab from '../src/components/menubar/Tab'
 import '@testing-library/jest-dom'
 
-describe('Home', () => {
-    it('It renders a set day of the month', () => {
-        render(<Tab day="Monday" active="Monday" onDayChange={() => { }} />)
+describe('Tab Component', () => {
+    test('renders a tab with specified name of the day', () => {
+        render(<Tab key={1} day={'Monday'} active={'Monday'} onDayChange={() => { }} />);
+        const tabElement = screen.getByText('Monday');
+        expect(tabElement).toBeInTheDocument();
+    });
 
-        const text = screen.getByText('Monday')
+    test('if it changing active state when the button was clicked', () => {
+        let active = 'Monday';
+        render(<Tab key={1} day={'Monday'} active={active} onDayChange={() => { active = 'Tuesday' }} />);
+        const tabElement = screen.getByText('Monday');
+        fireEvent.click(tabElement);
+        expect(active).toBe('Tuesday');
+    });
 
-        expect(text).toBeInTheDocument()
-    })
+    test('render correctly active class name', () => {
+        let className = 'flex cursor-pointer  duration-200 text-white font-bold justify-center shadow-sm self-center items-center w-1/5 hover:bg-green-700  rounded-t-2xl  h-full bg-green-700';
+        render(<Tab key={1} day={'Monday'} active={'Monday'} onDayChange={() => { }} />);
+        const tabElement = screen.getByText('Monday');
+        expect(tabElement).toHaveAttribute('class', className)
+    });
+    
+    test('render correctly active class name', () => {
+        let className = 'flex cursor-pointer  duration-200 text-white font-bold justify-center shadow-sm self-center items-center w-1/5 hover:bg-green-700  rounded-t-2xl  h-full bg-green-700';
+        render(<Tab key={1} day={'Monday'} active={'Friday'} onDayChange={() => { }} />);
+        const tabElement = screen.getByText('Monday');
+        expect(tabElement).not.toHaveAttribute('class', className)
+    });
 })
