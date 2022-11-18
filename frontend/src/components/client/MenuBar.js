d7951020e17485efea97182eef39c31eea24c428
Nekxis <nexispolska16@gmail.com>
2022-11-18 22:48:03 +0100

    ready

--- frontend/src/components/client/MenuBar.js
+++ frontend/src/components/client/MenuBar.js
@@ -13,6 +13,8 @@ export default function MenuBar() {
   const { cartItems } = useContext(MealsContext)
 
   const [activeDay, setActiveDay] = useState('');
+  const [open ,setOpen] = useState(false)
+
 
   const context = useContext(MealsContext)
 
@@ -24,14 +26,31 @@ export default function MenuBar() {
   return (
 
     <div className='flex flex-col p-5 lg:w-3/4 lg:h-2/3 sm:h-full sm:w-5/6 justify-center self-center shadow-xl'>
-      <div className='flex md:flex-row sm:flex-col w-full md:h-20 sm:h-128 self-center justify-around'>
+      <div className='md:flex sm:hidden w-full h-20 self-center justify-around'>
         {days.map((day, index) => {
           return (
             <Tab key={index} day={day} role='day' active={activeDay} onDayChange={setDayHandler} />
           )
         })}
       </div>
-      <div className='flex flex-col w-full h-full self-center overflow-y-scroll rounded-md'>
+      {/* Mobile version */}
+      <div
+        onClick={() => {setOpen(!open)}}
+        className="md:hidden sm:flex w-full my-2 justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-xl font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+        Day Selection
+      </div>
+
+        {open ?  <div className='md:hidden sm:flex flex-col w-full mb-2 h-128 self-center justify-around'>
+          {days.map((day, index) => {
+          return (
+            <Tab key={index} day={day} role='day' active={activeDay} onDayChange={setDayHandler} />
+          )
+        })}
+        </div> : null }
+
+
+      {/* End of mobile version */}
+      <div className='flex flex-col w-full h-full border-y-2 self-center overflow-y-scroll '>
         <Meal />
       </div>
       <button onClick={() => console.log(cartItems)} className='flex p-4 mt-5 duration-200 font-bold justify-center shadow-sm self-center items-center md:w-1/6 sm:w-28 rounded-2xl text-white bg-green-400 hover:bg-green-500 focus:bg-green-700'>Confirm</button>
--- frontend/src/components/shared-components/Tab.js
+++ frontend/src/components/shared-components/Tab.js
@@ -2,7 +2,7 @@
 export default function Tab({ day, onDayChange, active, role }) {
     return (
         <button role={role} onClick={() => { onDayChange(day) }}
-                className={`md:w-1/5 md:h-full sm:w-3/5 sm:h-1/5 sm:my-1 md:rounded-t-2xl md:rounded-b-sm sm:rounded-2xl flex duration-200 text-white font-bold justify-center shadow-sm self-center items-center
+                className={`md:w-1/5 md:h-full sm:w-2/3 sm:h-1/5 sm:my-1 md:rounded-t-2xl md:rounded-b-sm sm:rounded-2xl flex duration-200 text-white font-bold justify-center shadow-sm self-center items-center
                 ${active === day ? `bg-green-700 hover:bg-green-700 border border-gray md:text-2xl sm:text-xl` : 'bg-green-400 border md:text-xl sm:text-lg'}`}>
           {day}
         </button>
