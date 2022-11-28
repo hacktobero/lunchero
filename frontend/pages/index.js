8b33860f0f5b69d21cf922f8e7e41ee1b24b0f4b
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2022-11-28 21:27:42 +0100

    essa

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,14 @@
 import { useEffect, useState } from "react";
 import Navbar from "../src/components/shared-components/navbar";
 import MenuBar from "../src/components/client/MenuBar";
-
+import { createUser } from '../client-api/createUser';
 export default function Home() {
 
+  useEffect(() => {
+      createUser('string1234@string.pl', 'string1223')
+      .then((user) => console.log(user))
+  }, []);
+  
   return (
     <div className={'flex w-full h-screen items-center justify-start flex-col'}>
       <Navbar/>
