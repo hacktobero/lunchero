c601a1fa733c59e912af27aa17d0dba4ce8a790b
Yamcz00r <Dawid.Jamka@onet.pl>
2022-12-03 21:32:03 +0100

    commit-1

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -7,7 +7,7 @@ import { getUserByToken } from '../client-api/getUserByToken';
 import { deleteUser } from '../client-api/deleteUser';
 import { updateUserEmail } from '../client-api/updateUserEmail';
 export async function getServerSideProps() {
-  const response = await updateUserEmail('test333@test333.pl', 'haslohaslo12');
+  const response = await generateToken('test333@test333.pl', 'haslohaslo12');
 
   return {
     props: {
@@ -21,13 +21,7 @@ export default function Home({ response }) {
 
   console.log(response)
 
-  useEffect(() => {
-    const asyncFn = async () => {
-      const res = await updateUserEmail(19, 'test4445@test444.pl' ,response.access_token);
-      console.log(res);
-    };
-    asyncFn();
-  }, []);
+
   
   return (
     <div className={'flex w-full h-screen items-center justify-start flex-col'}>
