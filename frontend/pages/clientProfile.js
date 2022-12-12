b83bb29f6c969988c22c43857342ac389f8ce425
pawelcp <94795184+pawelcp@users.noreply.github.com>
2022-12-12 21:13:13 +0100

    logout

--- frontend/pages/clientProfile.js
+++ frontend/pages/clientProfile.js
@@ -17,6 +17,7 @@ export default function profile() {
         .then((user) => setEmail(user.email))
     })
     console.log(email);
+
     return(
         <div className='flex-col flex items-center lg:mt-14 sm:mt-2'>
                 <div className='flex self-center justify-center mt-16'>
@@ -25,17 +26,17 @@ export default function profile() {
                     </IconContext.Provider>
                 </div>
                 <h2 role='email' className='self-center justify-center lg:text-xl sm:text-md mt-5 text-center'>{email}</h2>
-                <div className='flex flex-col w-full items-center mt-16'>
-                    <h2>Preferences</h2>
-                    <select required className='justify-center flex mt-1 rounded-lg text-center text-black border-2 border-green-500 sm:w-1/2 lg:w-1/5  outline-none p-2 shadow-xl hover:border-green-600'>
+                <div className='flex flex-col w-full mt-16'>
+                    <h2 className='self-center'>Preferences</h2>
+                    <select required className='justify-center self-center mt-1 rounded-lg text-center text-black border-2 border-green-500 sm:w-1/2 lg:w-1/5  outline-none p-2 shadow-xl hover:border-green-600'>
                         <option value=''>{/*user Preferences*/}</option>
                         <option value='Meat'>Meat</option>
                         <option value='Vege'>Vege</option>
                         <option value='Keto'>Keto</option>
                     </select>
-                    <button className="drop-shadow-xl text-white mt-2 px-2 py-2 bg-green-600 rounded-lg hover:bg-green-700 ">Submit</button>
+                    <button className="drop-shadow-xl self-center text-white mt-2 px-2 py-2 bg-green-600 rounded-lg hover:bg-green-700 ">Submit</button>
                 </div>
-                <button onClick={ () => { router.push('/') }} className="mt-40 drop-shadow-xl text-white px-6 py-4 bg-green-600 rounded-lg hover:bg-green-700 ">Log out</button>
+                <button onClick={ () => { router.reload(); localStorage.removeItem('luncheroToken') }} className="mt-40 drop-shadow-xl text-white px-6 py-4 bg-green-600 rounded-lg hover:bg-green-700 ">Log out</button>
         </div>
     )
 }
