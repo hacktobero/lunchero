d8d83e67bc7e01783163299699f2b995ac59e1b9
Nekxis <nexispolska16@gmail.com>
2022-12-15 21:25:33 +0100

    ready

--- frontend/cypress/e2e/dropdown.cy.js
+++ frontend/cypress/e2e/dropdown.cy.js
@@ -1,21 +1,21 @@
 import Dish from "../../src/components/client/meals-section/Dish";
 
 describe('dropdowns', () => {
-  it('Breakfast', () => {
 
-    cy.Login('test@gmail.com', '123456789')
+  it('Breakfast', () => {
+    cy.Login('test@test.com', '12345678')
     cy.get('button[name="Breakfast"]').click()
     cy.contains('Ingredients:')
   })
-  it('Lunch', () => {
 
-    cy.Login('test@gmail.com', '123456789')
+  it('Lunch', () => {
+    cy.Login('test@test.com', '12345678')
     cy.get('button[name="Lunch"]').click()
     cy.contains('Ingredients:')
   })
-  it('Extra', () => {
 
-    cy.Login('test@gmail.com', '123456789')
+  it('Extra', () => {
+    cy.Login('test@test.com', '12345678')
     cy.get('button[name="Extra"]').click()
     cy.contains('Ingredients:')
   })
