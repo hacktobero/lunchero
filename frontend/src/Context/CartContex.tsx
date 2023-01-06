5eda956317c7e3c1c4f5201334621d1996366df0
Yamcz00r <99492855+Yamcz00r@users.noreply.github.com>
2023-01-06 23:14:15 +0100

    changes

--- frontend/src/Context/CartContex.tsx
+++ frontend/src/Context/CartContex.tsx
@@ -1,37 +1,47 @@
 import { createContext, useState, useEffect } from "react";
-import { CartContextType, ProviderType, Meals, Cart } from '../@types/cart';
- 
+import { CartContextType, ProviderType, Meals, Cart } from "../@types/cart";
+
 export const CartContext = createContext<CartContextType | null>(null);
 
 const CartProvider = ({ children }: ProviderType) => {
-    const [mealsItems, setMealsItems] = useState<Meals[] | []>([]);
+  const [mealsItems, setMealsItems] = useState<Meals[] | []>([]);
 
-    const [cartItems, setCartItems] = useState<Cart[]>([]);
+  const [cartItems, setCartItems] = useState<Cart[]>([]);
 
+  useEffect(() => {
+    const getMeals = async () => {
+      const response = await fetch("/data.json");
+      const data = await response.json();
+      setMealsItems(data.menu.meals);
+    };
+    getMeals().catch((e) => console.log(e));
+  }, []);
 
-    useEffect(() => {
-        const getMeals = async () => {
-            const response = await fetch('/data.json');
-            const data = await response.json();
-            setMealsItems(data.menu.meals)
+  const increaseCartQuantity = (id: number) => {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id)) {
+        return [...currentItems, { id, quantity: 1 }];
+      } //Case where item dosen't already exists in cart
+      return currentItems.map((item) => {
+        if (item.id === id) {
+          return { ...item, quantity: item.quantity + 1 };
         }
-        getMeals().catch(e => console.log(e));
-    }, []);
+        return item;
+      });
+    });
+  };
+  
 
-    const increaseCartQuantity = (id: number) => {
-       
-    }
+  const initialState = {
+    mealsItems,
+    cartItems,
+    increaseCartQuantity,
 
+  };
 
-    const initialState = {
-        mealsItems,
-        cartItems,
-
-    }
-
-    return (
-        <CartContext.Provider value={initialState}>{children}</CartContext.Provider>
-    )
+  return (
+    <CartContext.Provider value={initialState}>{children}</CartContext.Provider>
+  );
 };
 
 export default CartProvider;
